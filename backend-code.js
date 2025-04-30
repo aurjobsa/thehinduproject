902ea39948da7d87678653db166ca80642127d94
aurjobsa <Aurjobsa@gmail.com>
2025-05-01 00:14:33 +0530

    Final

--- backend-code.js
+++ backend-code.js
@@ -1,6 +1,6 @@
 // server.js
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { Pool } = require('pg');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
